d1517472665b34aa35616314ef382be900df1fec
yejinp <ye895937@126.com>
2022-11-04 21:26:36 +0800

    add BottomUpCutRod

--- DynamicProgramming/MEMOIZED-CUT-ROD/MemoizedCutRod.go
+++ DynamicProgramming/MEMOIZED-CUT-ROD/MemoizedCutRod.go
@@ -31,6 +31,21 @@ func MemoizedCutRodAux(p, r []int) int {
 	return q
 }
 
+func BottomUpCutRod(p []int) int {
+	r := make([]int, len(p)+1)
+	r[0] = 0
+
+	for j := 1; j <= len(p); j++ {
+		q := math.MinInt
+		for i := 0; i < j; i++ {
+			q = max(q, p[i]+r[j-i-1])
+		}
+		r[j] = q
+	}
+
+	return r[len(p)]
+}
+
 func max(x, y int) int {
 	if x > y {
 		return x
--- DynamicProgramming/MEMOIZED-CUT-ROD/MemoizedCutRod_test.go
+++ DynamicProgramming/MEMOIZED-CUT-ROD/MemoizedCutRod_test.go
@@ -2,7 +2,7 @@ package memoizedCutRod
 
 import "testing"
 
-func TestCutRod(t *testing.T) {
+func TestMemoizedCutRod(t *testing.T) {
 	tests := []struct {
 		input []int
 		want  int
@@ -16,6 +16,26 @@ func TestCutRod(t *testing.T) {
 
 	for _, test := range tests {
 		get := MemoizedCutRod(test.input)
+		if test.want != get {
+			t.Errorf("cutRod(%v), got:%v, want:%v", test.input, get, test.want)
+		}
+	}
+}
+
+func TestBottomUpCutRod(t *testing.T) {
+	tests := []struct {
+		input []int
+		want  int
+	}{
+		{[]int{18, 90}, 90},
+		{[]int{1, 5}, 5},
+		{[]int{1, 5, 8}, 8},
+		{[]int{1, 5, 8, 9}, 10},
+		{[]int{1, 5, 8, 9, 17, 17, 20, 24, 30}, 30},
+	}
+
+	for _, test := range tests {
+		get := BottomUpCutRod(test.input)
 		if test.want != get {
 			t.Errorf("cutRod(%v), got:%v, want:%v", test.input, get, test.want)
 		}
