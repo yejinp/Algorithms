d456567dd2bb28ad80d46b8adee674e58865e4f6
yejinp <ye895937@126.com>
2022-11-27 16:54:16 +0800

    add longest common subsequence

---
+++ DynamicProgramming/LongestCommonSubsequence/lcsLength.go
@@ -0,0 +1,49 @@
+package lcs
+
+import "fmt"
+
+func lcsLength(x, y string) int {
+	lx, ly := len(x), len(y)
+	b, c := make([][]byte, lx), make([][]int, lx+1)
+
+	for i := 0; i < lx; i++ {
+		b[i] = make([]byte, ly)
+	}
+
+	for i := 0; i < lx+1; i++ {
+		c[i] = make([]int, ly+1)
+	}
+
+	for i := 0; i < lx; i++ {
+		for j := 0; j < ly; j++ {
+			if x[i] == y[j] {
+				c[i+1][j+1] = c[i][j] + 1
+				b[i][j] = '\\'
+			} else if c[i][j+1] >= c[i+1][j] {
+				c[i+1][j+1] = c[i][j+1]
+				b[i][j] = '^'
+			} else {
+				c[i+1][j+1] = c[i+1][j]
+				b[i][j] = '<'
+			}
+		}
+	}
+	//	fmt.Println(c[lx][ly])
+	return c[lx][ly]
+}
+
+func printLCS(b [][]byte, X string) {
+	i, j := len(b), len(X)
+	if i == 0 || j == 0 {
+		return
+	}
+
+	if b[i][j] == '\\' {
+		printLCS(b[:][:i-1], X[:j-1])
+		fmt.Printf("\\")
+	} else if b[i][j] == '^' {
+		printLCS(b[:][:i-1], X[:j-1])
+	} else {
+		printLCS(b[:][:], X[:j-1])
+	}
+}
---
+++ DynamicProgramming/LongestCommonSubsequence/lcsLength_test.go
@@ -0,0 +1,49 @@
+package lcs
+
+import "testing"
+
+func equal(a []byte, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if b[i] != v {
+			return false
+		}
+	}
+	return true
+}
+
+func equal2(a [][]byte, b [][]byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if !equal(v, a[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func TestLcsLength(t *testing.T) {
+	tests := []struct {
+		X    string
+		Y    string
+		want int
+	}{
+		{"ABCD", "ACBD", 3},
+		{"BDCABA", "ABCBDAB", 4},
+		{"ACCGGTCGAGTGCGCGGAAGCCGGCCGAA", "GTCGTTCGGAATGCCGTTGCTCTGTAAA", 20},
+	}
+
+	for _, test := range tests {
+		got := lcsLength(test.X, test.Y)
+		if got != test.want {
+			t.Errorf("lcsLength(%v,%v) got %v, want:%v",
+				test.X, test.Y, got, test.want)
+		}
+	}
+}
